636b43d526404c8f37355ce4c13c726a916dc597
Regy Eduardo <regyeduardo@gmail.com>
2021-03-05 17:43:04 -0300

    Functional Programming 001-006

---
+++ JavaScript Algorithms and Data Structures/08 Functional Programming/006 teste.js
@@ -0,0 +1,29 @@
+// The global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add (bookList, bookName) {
+  list = [...bookList]
+  list.push(bookName);
+  return list;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove (bookList, bookName) {
+  list = [...bookList]
+  var book_index = list.indexOf(bookName);
+  if (book_index >= 0) {
+    list.splice(book_index, 1);
+    return list;
+
+    // Change code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
