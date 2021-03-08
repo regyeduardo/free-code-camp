374b07deef199439053aeb8ae6728cd4c31b3658
Regy Eduardo <regyeduardo@gmail.com>
2021-03-08 18:48:15 -0300

    Functional Programmin 007-015

---
+++ JavaScript Algorithms and Data Structures/08 Functional Programming/010 test.js
@@ -0,0 +1,15 @@
+// The global variable
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  // Only change code below this line
+  var newArray = [] 
+//   this.forEach(item => newArray.push(callback(item)))
+  this.forEach(i => callback(i) ? newArray.push(i) : false)
+  // Only change code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
